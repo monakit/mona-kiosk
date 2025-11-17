4b05bcd076e3041639cf77b235bc494f30bab3e3
foxgem <jianhgreat@hotmail.com>
2025-11-17 11:58:50 +0900

    refresh the demo project

---
+++ packages/demo/src/content/config.ts
@@ -0,0 +1,16 @@
+import { defineCollection, z } from 'astro:content';
+import { PayableMetadata } from 'mona-kiosk';
+
+const blog = defineCollection({
+	type: 'content',
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			pubDate: z.coerce.date(),
+			updatedDate: z.coerce.date().optional(),
+			heroImage: image().optional(),
+		}).merge(PayableMetadata),
+});
+
+export const collections = { blog };
